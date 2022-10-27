8fb86b3bd936b099ecb29ef1d483c08750a9e251
Michal Radomski <michalra@op.pl>
2022-10-28 00:27:17 +0200

    Form was added

--- public/script.ts
+++ public/script.ts
@@ -1,1 +1,8 @@
-console.log("test");
+const cardForm = document?.querySelector("#cardForm");
+// console.log({ cardForm });
+
+cardForm?.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const cardNumber = parseInt((document?.querySelector("#card_number") as HTMLInputElement).value, 10);
+  console.log({ cardNumber });
+});
