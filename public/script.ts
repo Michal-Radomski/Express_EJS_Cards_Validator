5aa1e355abc3565f0dfa6f887ed20e8e560c82f3
Michal Radomski <michalra@op.pl>
2022-10-28 18:38:59 +0200

    Browser Validation done

--- public/script.ts
+++ public/script.ts
@@ -1,8 +1,37 @@
 const cardForm = document?.querySelector("#cardForm");
-// console.log({ cardForm });
+const cardNumberInput = document?.querySelector("#card_number") as HTMLInputElement;
+// console.log({ cardForm, cardNumberInput });
+
+window.onload = () => {
+  cardNumberInput.value = "";
+};
+cardNumberInput!.addEventListener("keydown", (event) => {
+  if (
+    event.key === "Enter" ||
+    event.key === "Escape" ||
+    event.key === "Backspace" ||
+    event.key === "CapsLock" ||
+    event.key === "Shift"
+  ) {
+    return;
+  }
+  if ((event.key >= "a" && event.key <= "z") || (event.key >= "A" && event.key <= "Z")) {
+    alert("Please enter only digits");
+    // console.log(event.key);
+  }
+});
+
+cardNumberInput!.addEventListener("input", (event) => {
+  (event.target! as HTMLInputElement).value = (event.target! as HTMLInputElement).value
+    .replace(/[^\dA-Z]/g, "")
+    .replace(/(.{4})/g, "$1 ")
+    .trim();
+});
 
 cardForm?.addEventListener("submit", (event) => {
   event.preventDefault();
-  const cardNumber = parseInt((document?.querySelector("#card_number") as HTMLInputElement).value, 10);
+  const cardNumberStringValue = cardNumberInput!.value;
+  // console.log({ cardNumberStringValue });
+  const cardNumber = parseInt(cardNumberStringValue.split(" ").join(""), 10);
   console.log({ cardNumber });
 });
